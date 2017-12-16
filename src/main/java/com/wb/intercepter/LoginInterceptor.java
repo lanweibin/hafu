6d33e36482b428b125bd9b325e609d7e5b205c77
lanweibin <lanweibin0203@outlook.com>
2017-12-16 14:20:59 +0800

    完成未完成的配置

---
+++ src/main/java/com/wb/intercepter/LoginInterceptor.java
@@ -0,0 +1,40 @@
+package com.wb.intercepter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import redis.clients.jedis.JedisPool;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+    @Autowired
+    private JedisPool jedisPool;
+
+    private List<String> excludedUrls;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+    }
+
+    public List<String> getExcludedUrls() {
+        return excludedUrls;
+    }
+
+    public void setExcludedUrls(List<String> excludedUrls) {
+        this.excludedUrls = excludedUrls;
+    }
+}
